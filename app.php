bf61e0962d3add267ad7bcac5c878a923c7f0355
ionvop <ionvop@gmail.com>
2026-07-08 13:05:34 +0800

    Update app.php

--- app.php
+++ app.php
@@ -6,7 +6,7 @@ require_once "api/config.php";
 
 <html>
     <head>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css?v=<?= time() ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
     </head>
