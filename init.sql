0cdbf1ce2343341955639c15ec60090f4bb4bf6b
ionvop <ionvop@gmail.com>
2026-07-12 14:22:56 +0800

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,3 @@
 CREATE TABLE `heat_locations` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `heat_index` REAL, "latitude" REAL, "longitude" REAL, `time` INTEGER DEFAULT (unixepoch()));
+CREATE TABLE "users" (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT, `email` TEXT UNIQUE, `avatar` TEXT, `session` TEXT, `time` INTEGER DEFAULT (unixepoch()));
+CREATE TABLE `posts` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `user_id` INTEGER REFERENCES `users`(`id`), `content` TEXT, `time` INTEGER DEFAULT (unixepoch()));
