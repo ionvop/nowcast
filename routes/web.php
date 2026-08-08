18dc5d87ab572273095d984fdd30d0162460bbc3
ionvop <ionvop@gmail.com>
2026-08-08 13:50:04 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::inertia('/', 'home')->name('home');
+Route::inertia('/heat', 'heat')->name('heat');
+Route::inertia('/map', 'map')->name('map');
+Route::inertia('/community', 'community')->name('community');
+Route::inertia('/profile', 'profile')->name('profile');
