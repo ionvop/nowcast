4fd2bfc969f53c5276eab4c70d1dfcbae2b9f1e8
ionvop <ionvop@gmail.com>
2026-08-11 00:23:50 +0800

    Create CoordinateRequest.php

---
+++ backend/app/Http/Requests/CoordinateRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * @property float $latitude
+ * @property float $longitude
+ */
+class CoordinateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'latitude' => ['required', 'numeric', 'between:-90,90'],
+            'longitude' => ['required', 'numeric', 'between:-180,180'],
+        ];
+    }
+}
