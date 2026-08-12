048baf40ecf178fdbeedefa3e7c255d8a4cd7937
ionvop <ionvop@gmail.com>
2026-08-13 00:33:17 +0800

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::any('/app/{any}', function () {
+    return File::get(public_path('app/index.html'));
+})->where('any', '.*');
