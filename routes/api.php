08569da9d3893fe49ecc06053972a763cd89be0a
ionvop <ionvop@gmail.com>
2026-08-19 17:18:39 +0800

    Match branch 'v3'

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\WeatherController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/weather', [WeatherController::class, 'weather']);
-Route::post('/geocode', [WeatherController::class, 'geocode']);
-Route::post('/forecast', [WeatherController::class, 'forecast']);
--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::inertia('/', 'home')->name('home');
-Route::inertia('/heat', 'heat')->name('heat');
-Route::inertia('/map', 'map')->name('map');
-Route::inertia('/community', 'community')->name('community');
-Route::inertia('/profile', 'profile')->name('profile');
