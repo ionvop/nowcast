08c204d7f844e45f0e646ffe6d7c2044294132a3
ionvop <ionvop@gmail.com>
2026-08-19 12:06:29 +0800

    Update build.gradle.kts

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -22,7 +22,7 @@ fun dartDefine(key: String): String {
 }
 
 android {
-    namespace = "com.example.test"
+    namespace = "com.ionvop.nowcast"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -33,7 +33,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.test"
+        applicationId = "com.ionvop.nowcast"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
