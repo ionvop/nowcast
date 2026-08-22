f0747cafa3219f920454eb7987536e88948af9a0
ionvop <ionvop@gmail.com>
2026-08-22 22:20:12 +0800

    Update heat_alert_service.dart

--- frontend/lib/src/services/heat_alert_service.dart
+++ frontend/lib/src/services/heat_alert_service.dart
@@ -22,8 +22,7 @@ const double kDefaultHeatAlertThreshold = 32.0;
 
 /// How often the background service re-checks the heat index and refreshes
 /// the persistent notification.
-/// TODO: Currently set to 15 seconds for testing. Should be in minutes.
-const Duration kHeatAlertCheckInterval = Duration(seconds: 15);
+const Duration kHeatAlertCheckInterval = Duration(minutes: 15);
 
 /// Notification channel used by the heat-alert service.
 const String kHeatAlertChannelId = 'heat_alerts';
