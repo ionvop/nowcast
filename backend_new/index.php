248356422b33628c6829d4e21e7dfcb0c86e9710
ionvop <ionvop@gmail.com>
2026-09-08 10:47:40 +0800

    update

--- backend_new/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once "common.php";
-header("Content-Type: application/json");
-echo json_encode(["message" => "Hello, world!"]);
--- backend_new/tools/schema.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE "tasks" (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `task` TEXT NOT NULL, `is_completed` INTEGER DEFAULT 0, `created_at` TEXT DEFAULT (datetime()));
