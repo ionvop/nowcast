cabb5150a23ba5be1429649302416de9e41ce24e
ionvop <ionvop@gmail.com>
2026-09-21 21:35:05 +0800

    Update Post.php

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 #[Fillable(['content', 'address', 'latitude', 'longitude'])]
 class Post extends Model
@@ -32,5 +33,13 @@ class Post extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The post's comments.
+     */
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
